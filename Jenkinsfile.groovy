229042df8a41c7a90707a18901bc6fb9410da385
Michael6138 <natanpopov6138@gmail.com>
2022-07-09 15:06:35 +0300

    adasda

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,8 +2,8 @@ pipeline {
     agent { docker { image 'maven:3.8.4-openjdk-11-slim' } }
     stages {
         stage('build') {
+            echo 'hi 777777777777777777'
             steps {
-                readFile('C:\\Users\\Michael\\check_clone\\repo_CI_CD_1\\Jenkinsfile.groovy')
                 sh 'mvn --version'
             }
         }
